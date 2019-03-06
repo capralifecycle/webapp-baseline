6af60209735b5e1114e9e89fed06c3fd456f4ab9
Henrik Steen <henrist@henrist.net>
2019-03-06 19:28:40 +0100

    Remove obsolete options to CleanWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
