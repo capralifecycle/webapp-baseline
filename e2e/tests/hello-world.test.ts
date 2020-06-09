9b0e48b34e0fa3000e7678cd9b1c17a8fb5dffe0
Henrik Steen <henrist@henrist.net>
2020-06-09 21:08:34 +0200

    Fix breaking e2e test

--- e2e/tests/hello-world.test.ts
+++ e2e/tests/hello-world.test.ts
@@ -2,20 +2,8 @@ import { Selector } from 'testcafe';
 
 fixture`Example app`.page('http://localhost:3000');
 
-class LandingPage {
-  public headline: Selector;
-
-  public constructor() {
-    this.headline = Selector('h1');
-  }
-
-  public getHeadlineText = () => this.headline.textContent;
-}
-
-const landingPage = new LandingPage();
-
-test('Check welcome message', async (t) => {
+test('Check environment message', async (t) => {
   await t
-    .expect(landingPage.getHeadlineText())
-    .match(new RegExp('some-app-some-version'));
+    .expect(Selector('div').textContent)
+    .match(new RegExp('Current environment LOCAL'));
 });
