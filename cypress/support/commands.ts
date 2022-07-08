52addcc27587b2c2f6729f4bd7e8c6c387e228a5
Truls <truls.stenrud@gmail.com>
2022-07-08 14:29:32 +0200

    Linting

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-call
 const compareSnapshotCommand = require("cypress-image-diff-js/dist/command");
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 compareSnapshotCommand();
