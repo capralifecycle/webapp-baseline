8b4e2fab3f77ab98da22b0d67a3c1d21ff65c3c9
Truls <truls.stenrud@gmail.com>
2022-07-08 14:20:37 +0200

    Get image-diff to work

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-import compareSnapshotCommand from "cypress-image-diff-js/dist/command";
-
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const compareSnapshotCommand = require("cypress-image-diff-js/dist/command");
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 compareSnapshotCommand();
