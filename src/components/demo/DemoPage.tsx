f96df7f8a9381bd7548ad1ab1c71627f1ac10ca1
Forat Seif <foratseif@gmail.com>
2023-08-07 21:58:29 +0200

    fix DemoPage css

--- src/components/demo/DemoPage.tsx
+++ src/components/demo/DemoPage.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "./reset-body-margin.css";
 import Card from "./Card";
 import Button from "./Button";
 import Center from "./Center";
@@ -12,20 +11,31 @@ const DemoPage: React.FC<Props> = () => {
   const [count, setCount] = React.useState(0);
 
   return (
-    <Center>
-      <Card width={300} height={110}>
-        <center>
-          <p>Count is currently {count}.</p>
-          <Button
-            onClick={() => {
-              setCount(count + 1);
-            }}
-          >
-            Click me!
-          </Button>
-        </center>
-      </Card>
-    </Center>
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        backgroundColor: "#fcfcfc",
+      }}
+    >
+      <Center>
+        <Card width={300} height={110}>
+          <center>
+            <p>Count is currently {count}.</p>
+            <Button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              Click me!
+            </Button>
+          </center>
+        </Card>
+      </Center>
+    </div>
   );
 };
 
