846d9af5d5772ba5e9d1d4c5bbde727ed781e202
Forat Seif <foratseif@gmail.com>
2023-09-21 12:02:22 +0200

    only button-danger should fail

--- gogi.sh
+++ gogi.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker run -v $PWD:/project -w /project --user pwuser --network host --mount type=bind,source=/$PWD/playwright_screenshots,target=/project/playwright_screenshots  --rm -it mcr.microsoft.com/playwright:v1.37.0-jammy /bin/bash
+docker run \
+  -v $PWD:/project \
+  -w /project \
+  --user pwuser \
+  --network host \
+  --mount type=bind,source=/$PWD/playwright_screenshots,target=/project/playwright_screenshots \
+  --rm \
+  -it mcr.microsoft.com/playwright:v1.37.0-jammy \
+  npm run ladle:test:update
