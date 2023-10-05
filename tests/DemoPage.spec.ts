3d315ebfc24635f575295cc0ff888ccd19e2cc34
Forat Seif <foratseif@gmail.com>
2023-10-05 15:00:04 +0200

    I am the best

--- tests/DemoPage.spec.ts
+++ tests/DemoPage.spec.ts
@@ -3,13 +3,13 @@ import { test } from "./ladle-helpers";
 
 test("DemoPage", async ({ getStory }) => {
   const { page, name } = await getStory(["Demo page", "Default story"]);
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await page.getByRole('button', { name: 'Click me!' }).click();
-  await expect(page.getByText('Count is currently')).toHaveText("Count is currently 8.")
+  await page.getByRole("button", { name: "Click me!" }).click();
+  await page.getByRole("button", { name: "Click me!" }).click();
+  await page.getByRole("button", { name: "Click me!" }).click();
+  await page.getByRole("button", { name: "Click me!" }).click();
+  await page.getByRole("button", { name: "Click me!" }).click();
+  await page.getByRole("button", { name: "Click me!" }).click();
+  await expect(page.getByText("Count is currently")).toHaveText(
+    "Count is currently 8.",
+  );
 });
