4e35f8020cd4af3846c0ecf09f578c0072cdb49d
Forat Seif <foratseif@gmail.com>
2023-10-05 12:28:54 +0200

    update

--- run_playwright_in_docker.sh
+++ run_playwright_in_docker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+command="ladle:playwright"
+
+if [ "$1" == "update" ] ; then
+  command="ladle:playwright:update"
+fi
+
 docker run \
   -v $PWD:/project \
   -w /project \
@@ -6,4 +13,4 @@ docker run \
   --mount type=bind,source=/$PWD/playwright_screenshots,target=/project/playwright_screenshots \
   --rm \
   -it mcr.microsoft.com/playwright:v1.37.0-jammy \
-  npm run ladle:test:update
+  npm run $command
