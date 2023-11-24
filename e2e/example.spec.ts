c637a7f7efcfadd2deaa3972b91b6cea8b3adad8
Truls <truls.stenrud@gmail.com>
2023-11-24 16:27:03 +0100

    Make lint happy

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,11 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe("start page", () =>{
-  test("should look similar as last time", async({page, context}) => {
+test.describe("start page", () => {
+  test("should look similar as last time", async ({ page }) => {
     await page.goto("/");
     await expect(page).toHaveScreenshot({
-      mask: [page.getByTestId("version-info")]
+      mask: [page.getByTestId("version-info")],
     });
   });
-})
-
+});
